f11b82e00455aa837f1e1c376e32aa75be895497
Conchel <conchelnyambita2@gmail.com>
2023-09-07 11:39:26 -0700

    C_language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main -prints the output by using the put instead of printf
+ *
+ * Return -always (0)
+ *
+ */
 
-int main()
-
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);

7e9a0fd13c6712b7d2f128c807ffbef131748f6d
Conchel <conchelnyambita2@gmail.com>
2023-09-07 11:49:05 -0700

    C_language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main -prints the output by using the put instead of printf
+ * main -it prints the output by using put
  *
- * Return -always (0)
+ * Return: always (0)
  *
  */
 int main(void)
